fd69d882df84e08ad8812a2d275a9fcc3a57ed27
Iarlyson Santana <Iarlyson.Santana@gmail.com>
2020-02-14 16:51:33 -0300

    Interface Emprestimo pronta

--- src/com/banco/bancodopovo/jgi/modelo/Emprestimo.java
+++ src/com/banco/bancodopovo/jgi/modelo/Emprestimo.java
@@ -1,4 +1,10 @@
 package com.banco.bancodopovo.jgi.modelo;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public interface Emprestimo {
+
+     boolean pedirEmprestimo(double valor) ;
+
 }
