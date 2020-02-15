83d1f1b72ea9e2a95ba24d7e836121c586f1f768
joaovmvini <44440717+joaovmvini@users.noreply.github.com>
2020-02-15 15:00:34 -0200

    Métodos de validação de userInputs em registro concluidos

--- src/com/banco/bancodopovo/jgi/controllers/RegisterController.java
+++ src/com/banco/bancodopovo/jgi/controllers/RegisterController.java
@@ -10,12 +10,10 @@ import javafx.scene.control.*;
 import javafx.event.ActionEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import javafx.scene.image.ImageView;
 import java.io.IOException;
 import java.time.LocalDate;
-
+import com.banco.bancodopovo.jgi.validations.Validations;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 
 
@@ -34,13 +32,18 @@ public class RegisterController {
     @FXML
     private TextField estadoInput;
     @FXML
-    private TextField cityInput;
+    private ComboBox selectCity;
     @FXML
     private DatePicker dateInput;
     @FXML
     private CheckBox ccBox;
     @FXML
     private CheckBox cpBox;
+
+    @FXML
+    private void initialize(){
+        selectCity.getItems().addAll("Cajazeiras","Uiraúna","JocaClaudino");
+    }
     @FXML
     void goBackToHome(MouseEvent event) throws IOException {
         Parent homeView = FXMLLoader.load(getClass().getResource("../telas/Home.fxml"));
@@ -58,9 +61,49 @@ public class RegisterController {
         String email = emailInput.getText();
         String pass = passInput.getText();
         String confirmPass = confirmPassInput.getText();
-        String date = dateInput.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        String city = cityInput.getText();
-        String estado = estadoInput.getText();
+        LocalDate date = dateInput.getValue();
+        String city = (String)selectCity.getValue();
+        String formattedDate;
+
+        //tratando o input do usuário
+
+        Boolean nameValidation = Validations.validarNome(name);
+        Boolean emailValidation = Validations.validarEmail(email);
+        int passValidation = Validations.validarSenha(pass,confirmPass);
+        int cpfValidation =  Validations.validarCPF(cpf,true);
+        String dateValidation = Validations.validarDate(date);
+
+        if(nameValidation){
+            System.out.println("Está tudo ok com o nome");
+        }
+
+        if(emailValidation) {
+            System.out.println("Está tudo ok com o email");
+        }
+
+        if(passValidation == 1){
+            System.out.println("Está tudo ok com a senha");
+        }
+
+        if(cpfValidation == 0){
+            System.out.println("Está tudo ok com o cpf");
+        }
+
+        if(dateValidation != null){
+            System.out.println("Está tudo ok com a data");
+        }
+
+        if(city != null) {
+            System.out.println("City is ok, city: "+ city);
+        }
+
+
+
+
+
+
+
+
 
     }
 }
