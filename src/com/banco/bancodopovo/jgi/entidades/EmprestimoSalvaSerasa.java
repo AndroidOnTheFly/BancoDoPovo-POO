06ac6223e867bf95f448957f2e1ff33e7d3e6a2a
Iarlyson Santana <Iarlyson.Santana@gmail.com>
2020-02-16 00:29:45 -0300

    Metodos de emprestimos aplicados e implementandos, 100% funcional

--- src/com/banco/bancodopovo/jgi/entidades/EmprestimoSalvaSerasa.java
+++ src/com/banco/bancodopovo/jgi/entidades/EmprestimoSalvaSerasa.java
@@ -1,19 +1,15 @@
 package com.banco.bancodopovo.jgi.entidades;
 
-import com.banco.bancodopovo.jgi.modelo.Conta;
 import com.banco.bancodopovo.jgi.modelo.Emprestimo;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
-public class EmprestimoSalvarAperto implements Emprestimo {
-    //private ContaCorrente contaCorrente;
-    private double valor = 1000;
-    private final double juros = 1.05;
+public class EmprestimoSalvaSerasa implements Emprestimo {
+    private double valor = 10000;
+    private final double juros = 1.30;
 
-
-    public EmprestimoSalvarAperto() {
-
+    public EmprestimoSalvaSerasa() {
     }
 
     @Override
@@ -25,7 +21,7 @@ public class EmprestimoSalvarAperto implements Emprestimo {
     @Override
     public boolean pagarPartedeEmprestimo(double valorPagamentoPacial, ContaCorrente contaCorrente) throws SQLException, IOException, ClassNotFoundException {
         if (valorPagamentoPacial <= contaCorrente.getSaldo()) {
-            if (valorPagamentoPacial < valor * juros){
+            if (valorPagamentoPacial < valor * juros) {
                 valor = valor * juros;
                 contaCorrente.realizarSaque(valorPagamentoPacial);
                 valor = valor - valorPagamentoPacial;
@@ -38,8 +34,7 @@ public class EmprestimoSalvarAperto implements Emprestimo {
                 System.out.println(valor);
                 return true;
             }
-        }
-        else{
+        } else {
             System.out.println("Você não tem saldo suficiente para fazer tal operação");
             return false;
         }
@@ -47,20 +42,15 @@ public class EmprestimoSalvarAperto implements Emprestimo {
 
     @Override
     public boolean pagarTotalDeEmprestimo(ContaCorrente contaCorrente) throws SQLException, IOException, ClassNotFoundException {
-        if (valor*juros<=contaCorrente.getSaldo()){
+        if (valor * juros <= contaCorrente.getSaldo()) {
             valor = valor * juros;
             contaCorrente.realizarSaque(valor);
             valor = valor - valor;
             System.out.println(valor);
             return true;
-        }
-        else {
+        } else {
             System.out.println("Saldo Insuficiente");
             return false;
         }
     }
 }
-
-
-
-
--- src/com/banco/bancodopovo/jgi/visao/App.java
+++ src/com/banco/bancodopovo/jgi/visao/App.java
@@ -1,14 +1,19 @@
 package com.banco.bancodopovo.jgi.visao;
 
+import com.banco.bancodopovo.jgi.entidades.ContaCorrente;
+import com.banco.bancodopovo.jgi.entidades.EmprestimoSalvarAperto;
 import com.banco.bancodopovo.jgi.entidades.Usuario;
 import com.banco.bancodopovo.jgi.enumeration.Cidade;
 import com.banco.bancodopovo.jgi.enumeration.TipoConta;
+import com.banco.bancodopovo.jgi.modelo.Conta;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 
@@ -19,13 +24,22 @@ public class App extends Application{
     private int maxWidth = 800;
     private int maxHeight = 584;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
         launch(args);
+        Usuario usuario = new Usuario("André", "07048150440", "Iarlyson.santana@outlook.com",
+                LocalDate.now(), "Paraíba", Cidade.Cajazeiras, TipoConta.Corrente, "123456");
 
-        Usuario usuario = new Usuario("Iarlyson", "07048150440", "iarlyson.santana@outlook.com", LocalDate.now(),
-                "Paraíba", Cidade.JocaClaudino, TipoConta.Corrente,"123456");
+        ContaCorrente contaCorrente = new ContaCorrente(usuario, Cidade.Cajazeiras);
+        System.out.println(contaCorrente.getSaldo());
+        EmprestimoSalvarAperto emprestimoSalvarAperto = new EmprestimoSalvarAperto();
+        emprestimoSalvarAperto.pegarEmprestimo(contaCorrente);
+        System.out.println(contaCorrente.getSaldo());
+        emprestimoSalvarAperto.pagarPartedeEmprestimo(500, contaCorrente);
+        System.out.println(contaCorrente.getSaldo());
+        contaCorrente.depositar(500);
+        emprestimoSalvarAperto.pagarTotalDeEmprestimo(contaCorrente);
 
-        System.out.println(usuario);
+
     }
 
 
